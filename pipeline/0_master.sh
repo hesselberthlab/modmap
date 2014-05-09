10c29a07902e9e8c5b95dca6813320b57e83e200
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-05-09 06:16:55 -0600

    update master loop

--- pipeline/0_master.sh
+++ pipeline/0_master.sh
@@ -6,6 +6,7 @@
 #BSUB -P storici
 
 <<DOC
+master analysis loop for storici mopmap pipeline
 DOC
 
 set -o nounset -o pipefail -o errexit -x
@@ -17,25 +18,28 @@ for assembly in ${ASSEMBLIES[@]}; do
 
     source $HOME/projects/collab/storici-lab/bin/config.sh
 
+    export ASSEMBLY=$assembly
     export BOWTIEIDX=$HOME/ref/genomes/$assembly/$assembly
     export CHROM_SIZES=$HOME/ref/genomes/$assembly/$assembly.chrom.sizes
     export GTF=$HOME/ref/genomes/$assembly/sgdGene.$assembly.gtf
     export FASTA=$HOME/ref/genomes/$assembly/$assembly.fa
     export RESULT=$HOME/projects/collab/storici-lab/results/common/$assembly
+    
+    job_dep="$ASSEMBLY[1-$NUM_SAMPLES]"
 
-    bsub -J align.$ASSEMBLY.dep \
+    bsub -J align.$job_dep \
         < $PIPELINE/1_align.sh
 
-    bsub -J coverage.$ASSEMBLY.dep -w align.$ASSEMBLY.dep \
+    bsub -J coverage.$job_dep -w align.$job_dep \
         < $PIPELINE/2_coverage.sh 
 
-    bsub -J nuc.freqs.$ASSEMBLY.dep -w coverage.$ASSEMBLY.dep \
+    bsub -J nuc.freqs.$job_dep -w coverage.$job_dep \
         < $PIPELINE/3_nuc_freqs.sh
 
-    bsub -J origin.anal.$ASSEMBLY.dep -w nuc.freqs.$ASSEMBLY.dep \
+    bsub -J origin.anal.$job_dep -w nuc.freqs.$job_dep \
         < $PIPELINE/4_origin_analysis.sh
 
-    bsub -J plots.$ASSEMBLY.dep -w origin.anal.$ASSEMBLY.dep \
+    bsub -J plots.$job_dep -w origin.anal.$job_dep \
         < $PIPELINE/5_plots.sh
 
 done
