a9904b00218b48789a6902c68421ecb4b0a1e8c1
jayhesselberth <jay.hesselberth@gmail.com>
2014-05-09 05:17:43 -0600

    added bsubs

--- pipeline/0_master.sh
+++ pipeline/0_master.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#BSUB -J master[1-3]
-#BSUB -e master.%J.%I.err
-#BSUB -o master.%J.%I.out
+#BSUB -J master
+#BSUB -e master.%J.err
+#BSUB -o master.%J.out
 #BSUB -q normal
 #BSUB -P storici
 
@@ -10,11 +10,12 @@ DOC
 
 set -o nounset -o pipefail -o errexit -x
 
-source $HOME/projects/collab/storici-lab/bin/config.sh
-
 ASSEMBLIES=("sacCer1" "sacCer2" "sacCer3")
+PIPLINE=$HOME/devel/modmap/pipeline
 
 for assembly in ${ASSEMBLIES[@]}; do
+
+    source $HOME/projects/collab/storici-lab/bin/config.sh
 
     export BOWTIEIDX=$HOME/ref/genomes/$assembly/$assembly
     export CHROM_SIZES=$HOME/ref/genomes/$assembly/$assembly.chrom.sizes
@@ -22,4 +23,18 @@ for assembly in ${ASSEMBLIES[@]}; do
     export FASTA=$HOME/ref/genomes/$assembly/$assembly.fa
     export RESULT=$HOME/projects/collab/storici-lab/results/common/$assembly
 
+    bsub -J align.dep \
+        < $PIPELINE/1_align.sh
+
+    bsub -J coverage.dep -w align.dep \
+        < $PIPELINE/2_coverage.sh 
+
+    bsub -J nuc.freqs.dep -w coverage.dep \
+        < $PIPELINE/3_nuc_freqs.sh
+
+    bsub -J origin.anal.dep -w nuc.freqs.dep \
+        < $PIPELINE/4_origin_analysis.sh
+
+    bsub -J plots.dep -w origin.anal.dep \
+        < $PIPELINE/5_plots.sh
 done
