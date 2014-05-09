b87ee9ac7412f0eacb9e8f004b7dc910ae383541
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-05-09 11:03:39 -0600

    updated to using master script

--- pipeline/config.sh
+++ pipeline/config.sh
@@ -1,16 +1,32 @@
 PROJECTID=storici-lab
-SAMPLES=(FS1 FS2 FS3)
-BOWTIEIDX=$HOME/ref/genomes/sacCer1/sacCer1
-RESULT=$HOME/projects/collab/storici-lab/results/common
-DATA=$HOME/projects/collab/storici-lab//data/common
-CHROM_SIZES=$HOME/ref/genomes/sacCer1/sacCer1.chrom.sizes
+
+SAMPLES=(FS1 FS2 FS3 FS4 FS5 FS6 FS7 FS8 FS9 FS10)
+NUM_SAMPLES=${#SAMPLES[@]}
+# SAMPLES and DESCRIPS must be in same order
+DESCRIPS=("YFP17, rnh1 rnh201 / RE"
+          "YFP17, rnh1 rnh201 / Fragmentase"
+          "YFP17, rnh1 rnh201 ung1"
+          "E134, rnh201"
+          "E134, pol3-5DV rnh201"
+          "E134, pol2-4 rnh201"
+          "E134, rnh201"
+          "YFP17, rnh201"
+          "YFP17, pol2-M644G rnh201"
+          "E134, rnh1 rnh201")
+
+#ASSEMBLY=sacCer2
+#BOWTIEIDX=$HOME/ref/genomes/$ASSEMBLY/$ASSEMBLY
+#RESULT=$HOME/projects/collab/storici-lab/results/common/$ASSEMBLY
+#FASTA=$HOME/ref/genomes/$ASSEMBLY/$ASSEMBLY.fa
+#CHROM_SIZES=$HOME/ref/genomes/$ASSEMBLY/$ASSEMBLY.chrom.sizes
+# GTF=$HOME/ref/genomes/$ASSEMBLY/sgdGene.$ASSEMBLY.gtf
+
+DATA=$HOME/projects/collab/storici-lab/data/common
 BIN=$HOME/devel/modmap/modmap
-RSCRIPTS=$BIN/R
+RSCRIPTS=$HOME/devel/modmap/R
 UMI=NNNNNNNN
 ALIGN_MODES=("uniq" "all")
 ALIGN_ARGS=("-m 1" "--all")
-GTF=$HOME/ref/genomes/sacCer1/sgdGene.sacCer1.gtf
-EXPPOS=$DATA/regev.exp.pos.bg
-EXPNEG=$DATA/regev.exp.neg.bg
-FASTA=$HOME/ref/genomes/sacCer1/sacCer1.fa
+EXPPOS=$DATA/$ASSEMBLY/regev.exp.pos.bg
+EXPNEG=$DATA/$ASSEMBLY/regev.exp.neg.bg
 METADATA=$DATA/metadata.tsv
