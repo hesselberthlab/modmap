49a15eacbaf21f4416b38ffc335367b1ef3610c1
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-07-21 12:39:11 -0600

    added chrM and 2mircon

--- pipeline/storici/background_nuc_freqs.sh
+++ pipeline/storici/background_nuc_freqs.sh
@@ -21,9 +21,39 @@ assembly=${ASSEMBLIES[$(($LSB_JOBINDEX - 1))]}
 output="$HOME/ref/genomes/$assembly/$assembly.genome.nuc.freqs.tab"
 FASTA="$HOME/ref/genomes/$assembly/$assembly.fa"
 
-python -m modmap.genome_nuc_freqs \
-    $FASTA \
-    --region-size-minimum 1 \
-    --region-size-maximum 3 \
-    --verbose \
-    > $output
+if [[ ! -f $output ]]; then
+    python -m modmap.genome_nuc_freqs \
+        $FASTA \
+        --region-size-minimum 1 \
+        --region-size-maximum 3 \
+        --verbose \
+        > $output
+fi
+
+output="$HOME/ref/genomes/$assembly/$assembly.chrM.nuc.freqs.tab"
+FASTA="$HOME/ref/genomes/$assembly/$assembly.chrM.fa"
+
+if [[ ! -f $output ]]; then
+    python -m modmap.genome_nuc_freqs \
+        $FASTA \
+        --region-size-minimum 1 \
+        --region-size-maximum 3 \
+        --only-chrom chrM \
+        --verbose \
+        > $output
+fi
+
+if [[ $assembly == "sacCer2" ]]; then
+    output="$HOME/ref/genomes/$assembly/$assembly.2micron.nuc.freqs.tab"
+    FASTA="$HOME/ref/genomes/$assembly/$assembly.2micron.fa"
+
+    if [[ ! -f $output ]]; then
+        python -m modmap.genome_nuc_freqs \
+            $FASTA \
+            --region-size-minimum 1 \
+            --region-size-maximum 3 \
+            --only-chrom 2micron \
+            --verbose \
+            > $output
+    fi    
+fi
