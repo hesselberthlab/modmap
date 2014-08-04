24ede53cb6624e61b0745393c64db68efd2a17bf
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-08-04 10:40:11 -0600

    added stivers pipeline

---
+++ pipeline/stivers/1_align.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#BSUB -J align[1-56]
+#BSUB -e align.%J.%I.err
+#BSUB -o align.%J.%I.out
+#BSUB -R "span[hosts=1]"
+#BSUB -q normal
+#BSUB -P stivers
+#BSUB -n 8
+
+<<DOC
+Trim the UMI from the FASTQ, align trimmed reads using bowtie suppressing 
+all reads that align more than once, then remove UMI duplicates from the
+alignment.
+DOC
+
+set -o nounset -o pipefail -o errexit -x
+
+source $HOME/projects/collab/stivers-lab/bin/config.sh
+sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+
+fastq=$DATA/$sample.fq.gz
+
+results=$RESULT/$sample/alignment
+if [[ ! -d $results ]]; then
+    mkdir -p $results
+fi
+
+threads=6
+
+for bwt_idx in ${!BOWTIEIDXS[@]}; do
+
+    bwt_index=${BOWTIEIDXS[$bwt_idx]}    
+    index_type=${BOWTIEIDX_TYPES[$bwt_idx]}
+
+    for aln_idx in ${!ALIGN_MODES[@]}; do
+
+        align_mode=${ALIGN_MODES[$aln_idx]}
+        align_arg=${ALIGN_ARGS[$aln_idx]}
+
+        if [[ $align_mode == "all" && $index_type == "hg19" ]]; then
+            continue
+        fi
+
+        bam=$results/$sample.$index_type.$align_mode.bam
+        stats=$results/$sample.$index_type.$align_mode.alignment.txt
+
+        # align the reads
+        if [[ ! -f $bam ]]; then
+            zcat $fastq \
+                | bowtie $align_arg -p $threads --sam $bwt_index - \
+                2> $stats \
+                | samtools view -ShuF4 - \
+                | samtools sort -o - $results/$sample.temp -m 8G \
+                > $bam
+            samtools index $bam
+        fi
+    done
+done
---
+++ pipeline/stivers/2_coverage.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+#BSUB -J coverage[1-56]%8
+#BSUB -e coverage.%J.%I.err
+#BSUB -o coverage.%J.%I.out
+#BSUB -q normal
+#BSUB -P stivers
+
+<<DOC
+Calculate coverage from bam files
+DOC
+
+set -o nounset -o pipefail -o errexit -x
+
+source $HOME/projects/collab/stivers-lab/bin/config.sh
+sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
+
+results=$RESULT/$sample
+bgresults=$RESULT/$sample/bedgraphs
+
+if [[ ! -d $bgresults ]]; then
+    mkdir -p $bgresults
+fi
+
+strands=(pos neg both)
+strand_args=("-strand +" "-strand -" "")
+
+for bwt_idx in ${!BOWTIEIDXS[@]}; do
+
+    bwt_index=${BOWTIEIDXS[$bwt_idx]}    
+    index_type=${BOWTIEIDX_TYPES[$bwt_idx]}
+
+    for aln_idx in ${!ALIGN_MODES[@]}; do
+
+        align_mode=${ALIGN_MODES[$aln_idx]}
+
+        bam=$results/alignment/$sample.$index_type.$align_mode.bam
+        stats=$results/alignment/$sample.$index_type.$align_mode.alignment.txt
+        
+        if [[ ! -f $bam ]]; then
+            echo ">> bam file not found: $bam" 1>&2
+            continue
+        fi
+
+        num_align=$(cat $stats | grep Reported | cut -f2 -d ' ')
+        scale_pm=$(echo "1 / ($num_align / 1000000)" | bc -l)
+
+        for strand_idx in ${!strands[@]}; do
+
+            strand=${strands[$strand_idx]}
+            strand_arg=${strand_args[$strand_idx]}
+
+            bedgraph=$bgresults/$sample.$index_type.$align_mode.strand.$strand.counts.bg
+            tab=$bgresults/$sample.$index_type.$align_mode.strand.$strand.counts.tab
+
+            bedtools genomecov -bg -g $CHROM_SIZES \
+                -ibam $bam $strand_arg -scale $scale_pm \
+                | gzip -c \
+                > $bedgraph
+
+            bedtools genomecov -d -g $CHROM_SIZES \
+                -ibam $bam $strand_arg -scale $scale_pm \
+                | gzip -c \
+                > $bedgraph
+
+            # create bigwigs
+            bigwig=$bgresults/$sample.$index_type.$align_mode.strand.$strand.counts.bw
+            
+            if [[ $index_type == "virus" ]]; then
+                chromsize=$CHROM_SIZES_VIRUS
+            elif [[ $index_type == "plasmid" ]]; then
+                chromsize=$CHROM_SIZES_PLASMID
+            else
+                chromsize=$CHROM_SIZES
+            fi
+
+            bedGraphToBigWig $bedgraph $chromsize $bigwig
+        done
+    done
+done
+
+
