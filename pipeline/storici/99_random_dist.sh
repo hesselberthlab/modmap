55950de621d360073dd3c14087f53d5502c5b65c
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-08-21 07:18:04 -0600

    moved to debug config

--- pipeline/storici/99_random_dist.sh
+++ pipeline/storici/99_random_dist.sh
@@ -11,14 +11,8 @@ DOC
 
 set -o nounset -o pipefail -o errexit -x
 
-# XXX testing
-#CONFIG=$HOME/devel/modmap/pipeline/storici/config.sh
-#ASSEMBLY=sacCer2
-#DEBUG="-debug"
-#RESULT=$HOME/projects/collab/storici-lab/results/common$DEBUG/$ASSEMBLY
-#CHROM_SIZES=$HOME/ref/genomes/$ASSEMBLY/$ASSEMBLY.chrom.sizes
-#LSB_JOBINDEX=1
-# XXX
+# XXX 
+# source debug-config.sh
 
 source $CONFIG
 sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
